56fd994d881c4e7461f899ff87aa08e87225d7d8
sulei1324 <651343980@qq.com>
2015-06-17 14:47:19 +0800

    ok

--- lzwCoding.py
+++ lzwCoding.py
@@ -1,21 +1,24 @@
 __author__ = 'Su Lei'
 
 # d = {'1': '1', '2':  '2', '3': '3', '4':  '4'}
-d = ['A', 'B', 'C', 'D']
-r = 'AAAAAAAAAAAAA'
+d = [str(x) for x in xrange(258)]
+r = ['255'] * 9
+s = '256'
+e = '257'
 
 o = []
 
 for i in xrange(len(r)):
     if i is 0:
+        o.append(d.index(s))
         pre = ''
         suf = r[0]
     else:
         suf = r[i]
-    print pre, suf
     if pre + suf in d:
         if i is len(r) - 1:
             o.append(d.index(pre + suf))
+            o.append(d.index(e))
         else:
             pre += suf
     else:
@@ -23,11 +26,27 @@ for i in xrange(len(r)):
         if i is len(r) - 1:
             o.append(d.index(pre))
             o.append(d.index(suf))
+            o.append(d.index(e))
         else:
             o.append(d.index(pre))
             pre = suf
 
 
 print o
-print d
+obs = ''
+for i in o:
+    tmp = bin(i)[2::]
+    if len(tmp) is not 9:
+        nt = 9 - len(tmp)
+        tmp = '0' * nt + tmp
+    obs += tmp
+
+if len(obs) % 8 is not 0:
+    n = len(obs) / 8
+    m = 8 * (n + 1) - len(obs)
+    obs += '0' * m
+
+y = [hex(int(obs[i: i + 8], 2)) for i in xrange(0, len(obs), 8)]
+print [obs[i: i + 8] for i in xrange(0, len(obs), 8)]
+print y
 
